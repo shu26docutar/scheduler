139339efd1104052712571c3522119e03b553b3b
Shuhei Hattori <62091655+shu26docutar@users.noreply.github.com>
2021-02-02 14:37:56 +0900

    ルーム機能の実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,25 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  has_many :room_users
+  has_many :rooms, through: :room_users
+  has_many :memos
+
+  with_options presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々]+\z/, message: '全角文字を使用してください' } do
+    validates :first_name
+    validates :last_name
+  end
+
+  with_options presence: true, format: { with: /\A[ァ-ヶ一-]+\z/, message: '全角カナを使用してください' } do
+    validates :first_name_kana
+    validates :last_name_kana
+  end
+
+  validates :password, presence: true, length: { minimum: 6 },
+            format: { with: /(?=.*[a-zA-Z])(?=.*\d)[a-zA-Z\d]{6,}/, message: 'は半角英数字で入力してください' }
+
+  with_options presence: true do
+    validates :nickname
+  end
 end
