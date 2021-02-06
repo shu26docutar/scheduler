bd40258dcca4feba8beae8bc7c65748bcff1089a
Shuhei Hattori <62091655+shu26docutar@users.noreply.github.com>
2021-02-06 11:30:57 +0900

    ルーティングの変更

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,9 @@
 Rails.application.routes.draw do
-  get 'events/index'
+  # get 'events/index'
   devise_for :users
   root to: "rooms#index"
-  resources :events, only: [:index, :new, :create]
   resources :rooms, only: [:new, :create] do
     resources :memos, only: [:index, :create]
   end
-  
+  resources :events, only: [:index, :new, :create]
 end
