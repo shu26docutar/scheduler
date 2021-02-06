580b9881895f4f454e845ea06e809eba998bd890
Shuhei Hattori <62091655+shu26docutar@users.noreply.github.com>
2021-02-06 15:06:42 +0900

    ユーザー機能

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
-  root to: "memos#index"
+  root to: "rooms#index"
+  resources :rooms, only: [:new, :create] do
+    resources :memos, only: [:index, :create]
+  end
+  resources :events, only: [:index, :new, :create]
+  resources :users, only: [:show]
 end
