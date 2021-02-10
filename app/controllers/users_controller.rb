667b4b3d2bc8b50901f3e4f93d42662bbae9b8be
Shuhei Hattori <62091655+shu26docutar@users.noreply.github.com>
2021-02-10 12:32:29 +0900

    ユーザー機能のバックエンド実装

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,28 @@
 class UsersController < ApplicationController
+    before_action :set_user, only: [:show, :edit]
+
     def show
+    end
+
+    def edit
+    end
+
+    def update
+        @user = User.find(params[:id])
+        if @user.update!(user_params)
+            redirect_to user_path(user_params)
+        else
+            render :edit
+        end
+    end
+
+    private
+    def set_user
         @user = User.find(params[:id])
     end
+    
+    def user_params
+        params.require(:user).permit(:nickname, :password,:email, :first_name, :last_name, :first_name_kana, :last_name_kana)
+    end
+    
 end
