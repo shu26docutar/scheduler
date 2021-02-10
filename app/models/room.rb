95187bb8231e4f51b08376657b328b386b7b3af1
Shuhei Hattori <62091655+shu26docutar@users.noreply.github.com>
2021-02-10 12:30:18 +0900

    ルームの削除機能の追加

--- app/models/room.rb
+++ app/models/room.rb
@@ -1,7 +1,7 @@
 class Room < ApplicationRecord
-    has_many :room_users
+    has_many :room_users, dependent: :destroy
     has_many :users, through: :room_users
-    has_many :memos
+    has_many :memos, dependent: :destroy
 
     validates :name, presence: true
 end
