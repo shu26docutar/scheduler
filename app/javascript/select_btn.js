37e8ade7e3600dca556abc3f750f2402769a42b0
Shuhei Hattori <62091655+shu26docutar@users.noreply.github.com>
2021-02-11 23:47:29 +0900

    スケジュール追加機能フロント実装追加

---
+++ app/javascript/select_btn.js
@@ -0,0 +1,20 @@
+//comment
+$(function () {
+    $('.btn-comment').on('click', () => {
+        $('#form-event-comment').show();
+    });
+});
+
+//place
+$(function () {
+    $('.btn-place').on('click', () => {
+        $('#form-event-place').show();
+    });
+});
+
+//url
+$(function () {
+    $('.btn-url').on('click', () => {
+        $('#form-event-url').show();
+    });
+});
