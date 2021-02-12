372648e5c8b45d74956a8f3e05b473bf3a3f95a5
Shuhei Hattori <62091655+shu26docutar@users.noreply.github.com>
2021-02-12 18:17:35 +0900

    リファクタリング

--- app/controllers/memos_controller.rb
+++ app/controllers/memos_controller.rb
@@ -1,19 +1,16 @@
 class MemosController < ApplicationController
-
+    before_action :set_room, only: [:index, :create]
     def index
         @rooms = Room.all
-        @memo = Memo.all
         @events = Event.all
         @memo = Memo.new
         @event = Event.new
-        @room = Room.find(params[:room_id]) 
         @memos = @room.memos.includes(:user)
     end
 
     def create
-        @room = Room.find(params[:room_id])
-        @memo = @room.memos.new(memo_params)
-        if @memo.save
+        @memos = @room.memos.new(memo_params)
+        if @memos.save
           redirect_to room_memos_path(@room)
         else
             render :index
@@ -23,5 +20,9 @@ class MemosController < ApplicationController
     private 
     def memo_params
         params.require(:memo).permit(:content).merge(user_id: current_user.id)
+    end
+
+    def set_room
+        @room = Room.find(params[:room_id])
     end
 end
