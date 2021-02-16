43e7756217f01577b909c3ff9fe74050eb91ad05
Shuhei Hattori <62091655+shu26docutar@users.noreply.github.com>
2021-02-16 12:08:50 +0900

    sns認証の導入バックエンド実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,28 +1,29 @@
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable, :validatable,
+         :omniauthable, omniauth_providers: %i[google_oauth2]
+
+         def self.from_omniauth(auth)
+          where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
+            user.name = auth.info.name
+            user.email = auth.info.email
+            user.password = Devise.friendly_token[0,20]
+          end
+        end
 
   has_many :room_users
   has_many :rooms, through: :room_users
   has_many :memos
   has_many :events
 
-  with_options presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々]+\z/, message: '全角文字を使用してください' } do
-    validates :first_name
-    validates :last_name
-  end
 
-  with_options presence: true, format: { with: /\A[ァ-ヶ一-]+\z/, message: '全角カナを使用してください' } do
-    validates :first_name_kana
-    validates :last_name_kana
-  end
+  # with_options presence: true, format: { with: /\A[ァ-ヶ一-]+\z/, message: '全角カナを使用してください' } do
+  #   validates :first_name_kana
+  #   validates :last_name_kana
+  # end
 
   validates :password, presence: true, length: { minimum: 6 },
             format: { with: /(?=.*[a-zA-Z])(?=.*\d)[a-zA-Z\d]{6,}/, message: 'は半角英数字で入力してください' }
 
-    validates :nickname, presence: true, allow_blank: true, on: :create
-
-    validates :password, presence: true, on: :create, allow_nil: true
+  #   validates :password, presence: true, on: :create, allow_nil: true
 end
