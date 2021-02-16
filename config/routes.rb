33284c3124109c5fcbc5bd686654875e8b97b509
Shuhei Hattori <62091655+shu26docutar@users.noreply.github.com>
2021-02-16 12:08:05 +0900

    ユーザー機能ルーティングの変更

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   # get 'events/index'
-  devise_for :users
+  devise_for :users, controllers: {omniauth_callbacks: "users/omniauth_callbacks"}
   root to: "rooms#index"
   resources :users, only: [:show, :edit, :update]
   resources :rooms, only: [:new, :create, :edit, :update, :destroy] do
